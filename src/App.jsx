484d5606d39fdd266bfbdc13f3ae08bd24d6337e
Christzxz <christnarvarte15@gmail.com>
2023-08-14 15:49:25 +0800

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { useState, useEffect } from 'react'
+import { API_URL } from './apiURL';
 import RandomMeme from './components/RandomMeme';
 
 
 function App() {
 
   const [getMemesData, setGetMemesData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function getData() {
       try {
-        const response = await fetch('https://api.imgflip.com/get_memes');
+        const response = await fetch(API_URL);
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        
         const memesData = await response.json();
         const memesArray = memesData.data.memes;
         
@@ -17,7 +24,7 @@ function App() {
         setGetMemesData(memesArray)
 
       } catch(error) {
-        console.log(error)
+        setError(error, 'An error occurred while fetching data');
       }
     }
 
@@ -26,7 +33,7 @@ function App() {
 
   return (
     <>
-      {getMemesData && <RandomMeme getMemesData={getMemesData} />}     
+      {error ? <div>{error}</div> : getMemesData && <RandomMeme getMemesData={getMemesData} />}     
     </>
   )
 }
